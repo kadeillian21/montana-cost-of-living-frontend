d5ffe9a6513882ca6a914dabffeb400e39eb6b97
Kade Illian <kadeillianmt@gmail.com>
2022-11-08 10:58:07 -0700

    added modal show without dynamic functionality

---
+++ src/CitiesModalShow.jsx
@@ -0,0 +1,15 @@
+export function CitiesModalShow(props) {
+  return (
+    <div>
+      <h2>{props.city.name}</h2>
+      <p>{props.city.description}</p>
+      <p>Median Income: ${props.city.median_income}</p>
+      <p>Median Rent with Utilities: ${props.city.median_gross_rent}</p>
+      <p>Population: {props.city.population}</p>
+      <p>Population Growth per Year: {props.city.population_percent_change}%</p>
+      <p>Median Monthly Mortgage: ${props.city.median_monthly_mortgage}</p>
+      <p>Price of Gas: ${props.city.gas_price}</p>
+      <p>Monthly Average Food Cost: ${props.city.monthly_average_food_cost}</p>
+    </div>
+  );
+}
