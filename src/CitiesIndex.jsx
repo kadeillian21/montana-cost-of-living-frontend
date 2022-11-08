e16ae88543ec1488aeff21b03937c984aa2e6aec
Kade Illian <kadeillianmt@gmail.com>
2022-11-08 10:54:30 -0700

    added cities index function and fixed a name bug on Footer

---
+++ src/CitiesIndex.jsx
@@ -0,0 +1,20 @@
+export function CitiesIndex(props) {
+  return (
+    <div>
+      <h1>Cities</h1>
+      {props.cities.map((city) => (
+        <div key={city.id}>
+          <h2>{city.name}</h2>
+          <p>{city.description}</p>
+          <p>Median Income: ${city.median_income}</p>
+          <p>Median Rent with Utilities: ${city.median_gross_rent}</p>
+          <p>Population: {city.population}</p>
+          <p>Population Growth per Year: {city.population_percent_change}%</p>
+          <p>Median Monthly Mortgage: ${city.median_monthly_mortgage}</p>
+          <p>Price of Gas: ${city.gas_price}</p>
+          <p>Monthly Average Food Cost: ${city.monthly_average_food_cost}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,23 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { CitiesIndex } from "./CitiesIndex";
+
 export function Home() {
+  const [cities, setCities] = useState([]);
+
+  const handleIndexCities = () => {
+    console.log("handleIndexCities");
+    axios.get("http://localhost:3000/cities.json").then((response) => {
+      console.log(response.data);
+      setCities(response.data);
+    });
+  };
+
+  useEffect(handleIndexCities, []);
+
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <CitiesIndex cities={cities} />
     </div>
   );
 }
