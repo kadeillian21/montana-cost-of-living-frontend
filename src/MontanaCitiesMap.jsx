41b043564723dfe30782b65473cbd3c5274ca395
Kade Illian <kadeillianmt@gmail.com>
2022-11-09 12:41:26 -0700

    removed search bar from nav bar

--- src/MontanaCitiesMap.jsx
+++ src/MontanaCitiesMap.jsx
@@ -7,19 +7,39 @@ mapboxgl.accessToken = import.meta.env.VITE_MONTANA_COST_OF_LIVING_TOKEN;
 export function MontanaCitiesMap() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-109.68642453848274);
+  const [lng, setLng] = useState(-110.1);
   const [lat, setLat] = useState(46.76675102746894);
-  const [zoom, setZoom] = useState(6);
+  const [zoom, setZoom] = useState(6.5);
+
+  // const layerList = document.getElementById("menu");
+  // const inputs = layerList.getElementsByTagName("input");
+
+  // for (const input of inputs) {
+  //   input.onclick = (layer) => {
+  //     const layerId = layer.target.id;
+  //     map.setStyle("mapbox://styles/mapbox/" + layerId);
+  //   };
+  // }
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
+      style: "mapbox://styles/kadeillian/cla9yhitx000214r35vvxrlu5",
       center: [lng, lat],
       zoom: zoom,
     });
   });
 
-  return <div ref={mapContainer} className="map-container"></div>;
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container"></div>
+      <div id="menu">
+        <input id="mapbox://styles/kadeillian/cla9yhitx000214r35vvxrlu5" type="radio" name="rtoggle" value="outdoors" />
+        <label for="custom-streets">Streets</label>
+        <input id="satellite-v9" type="radio" name="rtoggle" value="satellite" checked="checked" />
+        <label for="satellite-v9">Satellite</label>
+      </div>
+    </div>
+  );
 }
