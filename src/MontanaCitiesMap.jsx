6d6b14276baa48d32b9b07ea2837738aad16ebe3
Kade Illian <kadeillianmt@gmail.com>
2022-11-16 07:37:15 -0700

    added comments explaining MontanaCitiesMap

--- src/MontanaCitiesMap.jsx
+++ src/MontanaCitiesMap.jsx
@@ -7,16 +7,16 @@ mapboxgl.accessToken = import.meta.env.VITE_MONTANA_COST_OF_LIVING_TOKEN;
 export function MontanaCitiesMap() {
   const mapContainer = useRef(null);
   const map = useRef(null);
+  // lng, lat, and zoom are all essential variables for the map to load.
   const [lng, setLng] = useState(-110.1);
   const [lat, setLat] = useState(47);
   const [zoom, setZoom] = useState(6.25);
-
   const [cities, setCities] = useState([]);
 
+  // Returns the data from my backend and sets it to a useState variable so I can access it throughout the page.
   const handleIndexCities = () => {
     console.log("handleIndexCities");
     axios.get("http://localhost:3000/cities.json").then((response) => {
-      // console.log(response.data);
       setCities(response.data);
     });
   };
@@ -24,6 +24,7 @@ export function MontanaCitiesMap() {
   useEffect(handleIndexCities, []);
 
   useEffect(() => {
+    // The if condition makes the markers and associated popups if there is a current map.
     if (map.current) {
       cities.forEach((city) => {
         const popup = new mapboxgl.Popup({ offset: 25, className: "marker-popup" }).setHTML(`<h2>${city.name}</h1>
@@ -41,7 +42,7 @@ export function MontanaCitiesMap() {
       });
 
       return;
-    } // initialize map only once
+    } // Renders a new map if there is no map being rendered.  This is where the map should be rendered each time.
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/kadeillian/cla9yhitx000214r35vvxrlu5",
